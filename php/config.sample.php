6e166e66a9215fbe356adde4bdd97e9bec75363f
Lama Asiri <444008943@pnu.edu.sa>
2025-05-13 13:46:05 +0300

    sample

---
+++ php/config.sample.php
@@ -0,0 +1,8 @@
+<?php
+// Sample config file for local setup
+$host = "localhost";
+$user = "your_database_user";
+$pass = "your_database_password";
+$dbname = "your_database_name";
+$charset = 'utf8mb4';
+?>
